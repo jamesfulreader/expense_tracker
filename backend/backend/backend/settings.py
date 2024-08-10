53f0ae4d50cb66f3f5ee991e2f7afce462fbc47c
jfulreader <jamesfulreader@gmail.com>
2024-08-09 18:25:11 -0600

    create Entry model and serializer with user relationship setup settings file with rest_framework and cors

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -25,8 +25,13 @@ SECRET_KEY = 'django-insecure-zf+m%&1y%=z0vu(*9nlq2!fht9y4y2jc@5(@ymk=635tgx6^ib
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': [
+   'rest_framework.permissions.AllowAny',
+    ]
+}
 
 # Application definition
 
@@ -37,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -121,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWS_CREDENTIALS = True
